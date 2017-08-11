3616a7c8870b3f3ac06bde61193cb14b624f10a4
Stephan Losse <stephan.losse@me.com>
2017-08-11 13:41:58 +0200

    Delete init-bash.sh

--- init-bash.sh
+++
@@ -1,4 +0,0 @@
-git init --bare $HOME/.vimcfg
-alias vimconfig='/usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME'
-vimconfig config --local status.showUntrackedFiles no
-echo "alias vimconfig='/usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME'" >> $HOME/.bashrc
