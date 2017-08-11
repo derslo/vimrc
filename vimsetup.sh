298100296fa1adbf4b935ba25b1c0334caf57d2d
Stephan Losse <stephan.losse@me.com>
2017-08-11 14:37:31 +0200

    Update vimsetup.sh

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,22 +1,28 @@
-echo "alias vimconfig='/usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME'" >> $HOME/.bashrc
-echo ".vimcfg" >> .gitignore
-
 git clone --bare https://github.com/derslo/vimrc.git $HOME/.vimcfg
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-
-mkdir .vim/colors
-mkdir -p .vimconfig-backup
-
-curl -Lks https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim >> .vim/colors/solarized.vim
-
-/usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME checkout linux
 if [ $? = 0 ]; then
-  echo "Checked out config.";
+  echo "alias vimconfig='/usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME'" >> $HOME/.bashrc
+  echo ".vimcfg" >> .gitignore
+
+  git clone --bare https://github.com/derslo/vimrc.git $HOME/.vimcfg
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+  mkdir .vim/colors
+  mkdir -p .vimconfig-backup
+
+  curl -Lks https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim >> .vim/colors/solarized.vim
+
+  /usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME checkout linux
+  if [ $? = 0 ]; then
+    echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
     /usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME checkout linux 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .vimconfig-backup/{}
+  fi;
+  /usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME checkout linux
+  /usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME config status.showUntrackedFiles no
+
+  else
+  vimconfig pull origin linux
 fi;
-/usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME checkout linux
-/usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME config status.showUntrackedFiles no
 
 vim +PluginInstall +qall
