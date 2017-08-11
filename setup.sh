ca5c066c78d33b88d1dc66f58b43f4337fd97ebb
Stephan Losse <stephan.losse@me.com>
2017-08-11 12:06:12 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+git clone --bare https://github.com/derslo/vimrc.git $HOME/.vimcfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.vimcfg/ --work-tree=$HOME $@
+}
+mkdir -p .vimconfig-backup
+vimconfig checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    vimconfig checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .vimconfig-backup/{}
+fi;
+vimconfig checkout
+vimconfig config status.showUntrackedFiles no
